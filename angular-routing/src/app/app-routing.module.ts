96b4073677071b53516c7289c6144af79097b17e
lucachiere <chieregatti.luca@iisgalvanimi.edu.it>
2020-04-21 16:59:04 +0000

    ciao

--- angular-routing/src/app/app-routing.module.ts
+++ angular-routing/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { SearchComponent } from './search/search.component';
 import { TrackComponent } from './track/track.component';
+import { ArtistComponent } from './artist/artist.component';
 
 const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'search', component: SearchComponent },
   { path: 'tracks/:id', component: TrackComponent },
+  { path: 'artist/:id', component: ArtistComponent },
   { path: '',   redirectTo: '/search', pathMatch: 'full' }
 ];
 
