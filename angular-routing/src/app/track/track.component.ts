9dbb4123bf67f182b15bc129a01f7f97417e3993
lucachiere <chieregatti.luca@iisgalvanimi.edu.it>
2020-05-25 21:53:03 +0000

    boot

--- angular-routing/src/app/track/track.component.ts
+++ angular-routing/src/app/track/track.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Unit } from './unit.model';
 import { SpotifyService } from '../spotify.service';
 import { Observable } from 'rxjs';
 import { Location } from '@angular/common';
@@ -15,7 +17,19 @@ export class TrackComponent implements OnInit {
   routeObs: Observable<ParamMap>;
   spotifyServiceObs: Observable<Object>;
   track : any; //Qui salverò la traccia selezionata
+  postObserver : Observable<Object>;
+  postData : Object;
 
+  addUnit (newUnit: HTMLInputElement, newCost: HTMLInputElement, newHitSpeed: HTMLInputElement): boolean {
+      let newData = new Unit(newUnit.value, newCost.value, newHitSpeed.value);
+      let headers =  {headers: new HttpHeaders().set('Content-Type', 'application/json')};
+      //this.postObserver = this.http.post('https://3000-acb13a3a-bd01-4915-87cb-626d26b7ea44.ws-eu01.gitpod.io/songs', JSON.stringify(newData),headers)
+
+      //Meglio così ma da verificare
+      //this.postObserver = this.http.post('http://localhost:3000/users', newData);
+      this.postObserver.subscribe(data => this.postData = data);
+      return false;
+  }
   //Usiamo la dependency injection per farci mandare i moduli del routing e dello
   //SpotifyService
   constructor(
